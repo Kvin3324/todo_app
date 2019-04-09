45ab9c65b71436a53cf1d3a6158a212129fafaa2
Kévin Joya <kevin.joya@outlook.com>
2019-04-09 15:43:49 +0200

    A lot of shit but some good code

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,6 +16,6 @@ module TodoList
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # the framework and any gems in your application.    
   end
 end
